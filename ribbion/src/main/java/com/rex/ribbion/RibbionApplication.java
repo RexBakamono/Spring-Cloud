56bad5c9a8530cf485ff7ca98c6ad3a2706c8053
rekusu <36816097+RexBakamono@users.noreply.github.com>
2020-11-19 20:57:07 +0800

    ribbion

--- ribbion/src/main/java/com/rex/ribbion/RibbionApplication.java
+++ ribbion/src/main/java/com/rex/ribbion/RibbionApplication.java
@@ -2,11 +2,11 @@ package com.rex.ribbion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RibbionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RibbionApplication.class, args);
     }
